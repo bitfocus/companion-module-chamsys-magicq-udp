37bc6ad6f91795075f418668977237e9d907f24b
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:05 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'pb':      {
 			label:      'Set the playback fader level',
